84460d45b72c51b95e6c19fd475f55ea6d119b16
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-06-14 11:12:47 +0100

    created a link model

---
+++ lib/link.rb
@@ -0,0 +1,21 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+
+class Link
+
+include DataMapper::Resource
+
+property :id , Serial
+property :title , String
+property :url , String
+
+
+end
+
+#set up a connection with the database
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager")
+#checking that everything we wrote / the way we structured it is correct
+DataMapper.finalize
+#build the columsn and rows
+DataMapper.auto_upgrade!
