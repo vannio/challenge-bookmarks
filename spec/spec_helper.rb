6db5af9ec3ca0b07fd7f4b26d19601cfb298787c
Van Le <singularity@vannio.co.uk>
2016-06-14 13:07:04 +0100

    Added even more comments to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  # Everything in this block runs once before all the tests run
   config.before(:suite) do
     # For Rspec?
     DatabaseCleaner.strategy = :transaction
@@ -31,10 +32,12 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
+  # Everything in this block runs once before each individual test
   config.before(:each) do
     DatabaseCleaner.start
   end
 
+  # Everything in this block runs once after each individual test
   config.after(:each) do
     DatabaseCleaner.clean
   end
