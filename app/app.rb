eedeab82b7ab968e2f3a0f5cabca7fa16d3225d3
awye765 <awye765@googlemail.com>
2016-06-14 14:16:37 +0100

    Separating test and dev environments

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,14 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
 
   get '/links/new' do
     erb :'links/new'
