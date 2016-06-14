8cd084ea5b3eb1c41ced1337c25dee35c928573e
awye765 <awye765@googlemail.com>
2016-06-14 13:39:49 +0100

    Added database_cleaner

--- Gemfile
+++ Gemfile
@@ -2,6 +2,3 @@ source "https://rubygems.org"
 
 gem "data_mapper"
 gem "dm-postgres-adapter"
-
-
-BookmarkManager
--- spec/creating_links_spec.rb
+++ spec/creating_links_spec.rb
@@ -1,9 +1,14 @@
 feature "Creating new links" do
   scenario "Adding a new link" do
-    visit('/')
+    visit('/links/new')
     fill_in :title, with: "Makers Academy"
     fill_in :url, with: "http://www.makersacademy.com"
     click_button "Submit"
-    expect(page).to have_content "Title: Makers Academy"
+
+    expect(current_path).to eq('/links')
+
+    within "ul#links" do
+      expect(page).to have_content "Title: Makers Academy"
+    end
   end
 end
