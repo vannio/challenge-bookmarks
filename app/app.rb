5c4f032a5ec4c5988b853d7cf488092ea235a62f
Van Le <singularity@vannio.co.uk>
2016-06-15 14:35:42 +0100

    Added tag pages

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,3 @@
-
 ENV['RACK_ENV'] ||= "development"
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
@@ -17,7 +16,7 @@ class BookmarkManager < Sinatra::Base
   get '/links/new' do
   	erb(:'links/new')
   end
-  
+
   post '/links' do
   	#params[:url] and params[:title] both look at the name inputs in add.erb
   	link = Link.new(url: params[:url], title: params[:title])
@@ -25,6 +24,13 @@ class BookmarkManager < Sinatra::Base
   	link.tags << tag
   	link.save
   	redirect('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @name = params[:name]
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 
   run! if app_file == $0
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,9 @@
-class Tag 
+class Tag
 	include DataMapper::Resource
+
+	has(n, :links, through: Resource)
 
 	property :id, Serial
 	property :name, String
-	
+
 end
