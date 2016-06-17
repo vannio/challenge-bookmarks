062896b3d39fe9ebc274d215fd9291020c01a7cb
Van Le <singularity@vannio.co.uk>
2016-06-17 13:29:48 +0100

    Added some links to index when logged in

--- app/app.rb
+++ app/app.rb
@@ -30,10 +30,6 @@ class BookmarkManager < Sinatra::Base
   	erb :'links/index'
   end
 
-  get '/links/new' do
-  	erb :'links/new'
-  end
-
   post '/links' do
     link = Link.create(url: params[:url], title: params[:title])
     params[:tags].split(',').each do |name|
@@ -44,29 +40,35 @@ class BookmarkManager < Sinatra::Base
     redirect '/links'
   end
 
+  get '/links/new' do
+    erb :'links/new'
+  end
+
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
   end
 
-  get '/users/new' do
-   @user = User.new
-   erb :'users/new'
-  end
-
   post '/users' do
-    @user = User.create(email: params[:email],
-                      password: params[:password],
-                      password_confirmation: params[:password_confirmation])
+    @user = User.create(
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
 
     if @user.save
       session[:user_id] = @user.id
-      redirect '/links'
+      redirect '/'
     else
       flash.now[:errors] = @user.errors.full_messages
       erb :'users/new'
     end
+  end
+
+  get '/users/new' do
+   @user = User.new
+   erb :'users/new'
   end
 
   helpers do
